ba8c2a82e0959849ac3b4cba29390c6e2b1c2b38
Natã Alexandre Da Silva <natatgh@gmail.com>
2024-07-06 23:23:24 -0300

    New commit

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
+class Config:
+    DATABASE = 'jobs.db'
+    DEBUG = True
+
+app.config.from_object(Config)
+
 def get_jobs_from_db():
-    conn = sqlite3.connect('jobs.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, title, company, location, link, description FROM jobs")
-    jobs = cursor.fetchall()
-    conn.close()
-    return jobs
+    try:
+        with sqlite3.connect(app.config['DATABASE']) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                SELECT id, title, company, location, link, html_description, company_logo, tags
+                FROM jobs
+            """)
+            jobs = cursor.fetchall()
+        return jobs
+    except sqlite3.Error as e:
+        app.logger.error(f"Database error: {e}")
+        return []
 
 @app.route('/')
 def index():
@@ -17,4 +30,4 @@ def index():
     return render_template('index.html', jobs=jobs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=app.config['DEBUG'])
--- linkedin_jobs_descriptions.py
+++ linkedin_jobs_descriptions.py
@@ -1,7 +1,8 @@
 import sqlite3
-import requests
-from bs4 import BeautifulSoup
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
 def create_tables(db_path):
     conn = sqlite3.connect(db_path)
@@ -20,7 +21,8 @@ def create_tables(db_path):
             link TEXT,
             description TEXT,
             company_logo TEXT,
-            html_description TEXT
+            html_description TEXT,
+            tags TEXT
         )
     ''')
     conn.commit()
@@ -28,51 +30,47 @@ def create_tables(db_path):
 
 def get_job_details(job_url):
     try:
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        response = requests.get(job_url, headers=headers)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # Configurações do Selenium para rodar em modo headless
+        options = Options()
+        #options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
-        # Espera para garantir que todo o conteúdo seja carregado
-        time.sleep(3)
+        # Inicializa o driver do Chrome
+        driver = webdriver.Chrome(options=options)
 
-        # Printar o HTML completo da página
-        print("HTML da página:", soup.prettify())
+        driver.get(job_url)
+        time.sleep(20)  # Espera para garantir que todo o conteúdo seja carregado
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
         title_element = soup.select_one("h1.top-card-layout__title")
         company_element = soup.select_one("a.topcard__org-name-link")
         location_element = soup.select_one("span.topcard__flavor--bullet")
-        description_element = soup.select_one("div.jobs-box__html-content.jobs-description-content__text")
-        company_logo_element = soup.select_one("img.ivm-view-attr__img--centered")
+        company_logo_element = soup.select_one("img.artdeco-entity-image--square-5")
         
         title = title_element.get_text(strip=True) if title_element else "Título não encontrado"
         company = company_element.get_text(strip=True) if company_element else "Empresa não encontrada"
         location = location_element.get_text(strip=True) if location_element else "Localização não encontrada"
-        
-        # Captura o HTML completo dentro da div de descrição
-        description_div = soup.find("div", class_="jobs-box__html-content jobs-description-content__text")
-        html_description = str(description_div) if description_div else "Descrição HTML não encontrada"
-        description = description_div.get_text(strip=True) if description_div else "Descrição não encontrada"
-        
+
+        # Captura o HTML completo dentro do campo especificado pelo seletor CSS
+        description_container = soup.select_one('div.description__text.description__text--rich')
+        html_description = str(description_container) if description_container else "Descrição HTML não encontrada"
+        description = description_container.get_text(strip=True) if description_container else "Descrição não encontrada"
+
         company_logo = company_logo_element['src'] if company_logo_element else "Logotipo não encontrado"
 
-        # Debug prints
-        print(f"Job URL: {job_url}")
-        print(f"Title: {title}")
-        print(f"Company: {company}")
-        print(f"Location: {location}")
-        print(f"Description Element: {description_element}")
-        print(f"Description DIV: {description_div}")
-        print(f"Description (text): {description[:100]}")  # Print first 100 characters of description
-        print(f"Company Logo: {company_logo}")
-        print(f"HTML Description: {html_description[:100]}")  # Print first 100 characters of HTML description
+        # Captura as tags de critérios de emprego
+        criteria_list = soup.select("ul.description__job-criteria-list li")
+        tags = [li.get_text(strip=True) for li in criteria_list]
+        tags_text = "; ".join(tags) if tags else "Tags não encontradas"
 
-        return title, company, location, description, company_logo, html_description
+        driver.quit()
+        return title, company, location, description, company_logo, html_description, tags_text
     except Exception as e:
         print(f"Erro ao extrair os detalhes da vaga: {job_url}, erro: {e}")
-        return "", "", "", "", "", ""
+        return "Título não encontrado", "Empresa não encontrada", "Localização não encontrada", "Descrição não encontrada", "Logotipo não encontrado", "Descrição HTML não encontrada", "Tags não encontradas"
 
 def read_links_from_db(db_path):
     conn = sqlite3.connect(db_path)
@@ -89,11 +87,11 @@ def read_links_from_db(db_path):
     conn.close()
     return links
 
-def insert_job_into_db(db_path, title, company, location, link, description, company_logo, html_description):
+def insert_job_into_db(db_path, title, company, location, link, description, company_logo, html_description, tags):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO jobs (title, company, location, link, description, company_logo, html_description) VALUES (?, ?, ?, ?, ?, ?, ?)", 
-                   (title, company, location, link, description, company_logo, html_description))
+    cursor.execute("INSERT INTO jobs (title, company, location, link, description, company_logo, html_description, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+                   (title, company, location, link, description, company_logo, html_description, tags))
     conn.commit()
     conn.close()
 
@@ -101,10 +99,10 @@ def process_links(db_path, links):
     for job in links:
         job_id, link = job
         print(f"Processando a vaga com o link: {link}")
-        title, company, location, description, company_logo, html_description = get_job_details(link)
+        title, company, location, description, company_logo, html_description, tags = get_job_details(link)
         if title and company and description:
             print(f"Inserindo dados da vaga {job_id} no banco de dados.")
-            insert_job_into_db(db_path, title, company, location, link, description, company_logo, html_description)
+            insert_job_into_db(db_path, title, company, location, link, description, company_logo, html_description, tags)
             print(f"Dados da vaga {job_id} salvos com sucesso.")
         else:
             print(f"Falha ao extrair os dados para a vaga {job_id}")
